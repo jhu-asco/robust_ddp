8b7219abf720df51fe3ad64f47fc0bfc54fe5c40
Gowtham Garimella <ggarime1@jhu.edu>
2018-07-31 18:24:18 -0400

    Update robust ddp to use integrator

--- robust_ddp.py
+++ robust_ddp.py
@@ -6,12 +6,13 @@ import numpy as np
 
 class RobustDdp(Ddp):
     def __init__(self, dynamics, cost, us0, x0, dt, max_step, Sigma0=0,
-                 Sigmaw=0, use_prev_x=False):
+                 Sigmaw=0, use_prev_x=False, integrator=None):
         self.Sigmaw =Sigmaw
         self.Sigma = np.empty((cost.N+1, dynamics.n, dynamics.n))
         self.Sigma[0] = Sigma0
         super(RobustDdp, self).__init__(dynamics, cost, us0, x0, dt, max_step,
-                                        use_prev_x=use_prev_x)
+                                        use_prev_x=use_prev_x,
+                                        integrator=None)
 
     def update_dynamics(self, us, xs):
         self.V = 0
@@ -21,24 +22,16 @@ class RobustDdp(Ddp):
             x = xs[i]
             self.V = self.V + self.cost.stagewise_cost(i, x, u,
                                                        False, self.Sigma[i])
-            xdot = self.dynamics.xdot(i, x, u, self.w)
-            xs[i+1] = x + self.dt*xdot  # For now euler integration
+            xs[i + 1] = self.integrator.step(i, self.dt, x, u, self.w)
             SphericalObstacle.updatePreviousX(x)
             # TODO Change instead of dynamics take in an integrator that
             # integrates continuous dynamics using a fancy integrator maybe
-            jac = self.getDiscreteJacobians(i, x, u)
+            jac = self.integrator.jacobian(i, self.dt, x, u, self.w)
             Sigma = propagateEllipsoid(self.Sigma[i], self.Sigmaw, jac, K)
             self.Sigma[i+1] = Sigma
         self.V = self.V + self.cost.terminal_cost(xs[-1], False,
                                                   Sigma)
         SphericalObstacle.updatePreviousX(None)
-
-    def getDiscreteJacobians(self, k, x, u):
-        A, B, G = self.dynamics.jacobian(k, x, u, self.w)
-        A = np.eye(self.dynamics.n) + A*self.dt
-        B = B*self.dt
-        G = G*self.dt
-        return A, B, G
 
     def forward_pass_step(self, Ks, xs, us, alpha):
         Vnew = 0
@@ -48,13 +41,12 @@ class RobustDdp(Ddp):
             delta_x = x - self.xs[k]
             K_k = Ks[k]
             u = self.us[k] + alpha*K_k[:, -1] + np.dot(K_k[:, :-1], delta_x)
-            jac = self.getDiscreteJacobians(k, x, u)
-            xdot = self.dynamics.xdot(k, x, u, self.w)
+            jac = self.integrator.jacobian(k, self.dt, x, u, self.w)
             Vnew = Vnew + self.cost.stagewise_cost(k, x, u, False,
                                                    self.Sigma[k])
             Sigma = propagateEllipsoid(self.Sigma[k], self.Sigmaw, jac,
                                        K_k[:, :-1])
-            self.xs_up[k+1] = x + self.dt*xdot  # For now euler integration
+            self.xs_up[k+1] = self.integrator.step(k, self.dt, x, u, self.w)
             self.us_up[k] = u
             self.Sigma[k+1] = Sigma
             SphericalObstacle.updatePreviousX(x)
